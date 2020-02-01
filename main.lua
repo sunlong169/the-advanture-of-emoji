654ebd8874005a8eaf14061f49e3b31a5cb551eb
独不迁 <dubuqian@outlook.com>
2020-02-01 21:46:36 +0800

    changed the structure and added some new function

--- main.lua
+++ main.lua
@@ -1,19 +1,31 @@
 debug = true
+player = {x = 100, y = 280, speed = 300, img = nil}
+stage = {x = 0, y = 0, speed = 300, img = nil}
 
 function love.load()
-    player = love.graphics.newImage("/res/image/player/move1.png")
-    love.graphics.setBackgroundColor(50/255,175/255,1)
-    love.graphics.setLineWidth(5)
-    
+    player.img = love.graphics.newImage("/res/image/player/move1.png")
+    stage.img = love.graphics.newImage("res/image/stage/test-stage.png")
+    love.graphics.setBackgroundColor(50 / 255, 175 / 255, 1)
+    love.graphics.setLineWidth(5) 
 end
 
 function love.update(dt)
+    if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+    end
+
+    if love.keyboard.isDown('right', 'd') then
+        stage.x = stage.x - (stage.speed * dt)
+    end
+
+    if love.keyboard.isDown('left', 'a') then
+        stage.x = stage.x + (stage.speed * dt)
+    end
+
 end
 
 
 function love.draw()
-   
-    love.graphics.draw(player, 0,400-105,0,0.3,0.3)
-    love.graphics.line(0,400,800,400)
-    
+    love.graphics.draw(stage.img, stage.x, stage.y)
+    love.graphics.draw(player.img, player.x, player.y)
 end
