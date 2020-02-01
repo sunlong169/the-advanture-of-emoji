93a20bba2538814729fe23a93bdb885f1c44b06b
sthmo <41350633+southmorn@users.noreply.github.com>
2020-02-01 20:52:41 +0800

    Add files via upload

---
+++ tutorial _love.lua
@@ -0,0 +1,62 @@
+
+
+
+[[
+    对象
+--图片对象
+--love.graphics.newImage("图片路经")
+--img=love.graphics.newImage("图片路经")
+--给img复值，值为一张图片，图片的值需要此函数获取
+
+--文字对象
+--love.graphics.newFont("字体路径",大小)
+
+]]
+
+[[
+    绘制
+--图片绘制 love.graphics.draw(变量,x轴,y轴,偏转角度,宽度，高度)
+
+--文本打印 love,graphics.print("文本",x,y)
+
+--背景颜色设置 love.graphics.setBackgroundColor(R/255,G/255,B/255)
+
+--打印点 love.graphics.points(x,y,  x,y,  x,y,  x,y ... )
+
+--打印线 love.graphics.line(x,y,  x,y,  x,y,  x,y ... )
+
+--打印矩形 love.graphics.rectangle(形式（填充或者线条 分别为"fill"和"line"）,x,y,宽，高 )
+
+--打印圆形 love.graphics.circle(形式（填充或者线条 分别为"fill"和"line"）,x,y,宽，高）
+
+--打印三角形 love.graphics.circle(形式（填充或者线条 分别为"fill"和"line"）,x1,y1,x2，y2,x3,y3）
+--
+
+
+
+]]
+
+[[
+  绘图函数 
+  --设置点属性  love.graphics.setPointsSize(大小)
+  --设置线属性  love.graphics.setLineWidch(大小)
+   设置颜色 love.graphics.setColor(r/255,g/255,b/255,透明度)
+
+]]
+
+[[
+数学函数
+
+math.rad()
+弧度转角度
+
+]]
+
+
+
+
+--此函数为最先执行的函数
+function love.draw()
+    love.graphics.newImage
+    
+end
