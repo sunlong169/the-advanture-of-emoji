f22494cd7513adf9fecf6ed973d2d5f18dd3a1c4
sthmo <41350633+southmorn@users.noreply.github.com>
2020-02-01 20:51:41 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,19 @@
 debug = true
 
 function love.load()
-    player = love.graphics.newImage("/res/player.png")
+    player = love.graphics.newImage("/res/image/player/move1.png")
+    love.graphics.setBackgroundColor(50/255,175/255,1)
+    love.graphics.setLineWidth(5)
+    
 end
 
-function love.update()
+function love.update(dt)
 end
+
 
 function love.draw()
-    love.graphics.draw(player, 100, 100)
+   
+    love.graphics.draw(player, 0,400-105,0,0.3,0.3)
+    love.graphics.line(0,400,800,400)
+    
 end
