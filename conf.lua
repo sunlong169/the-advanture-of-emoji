71e5d47ea8180d9317479998c78e852bb32df8b2
sthmo <41350633+southmorn@users.noreply.github.com>
2020-02-01 20:50:28 +0800

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,8 +1,19 @@
-function love.conf(game)
-    game.title = "The Adventure of Emoji"
-    game.version = "11.3"
-    game.window.width = 640
-    game.window.height = 480
+debug = true
 
-    game.console = true;
+function love.load()
+    player = love.graphics.newImage("/res/image/player/move1.png")
+    love.graphics.setBackgroundColor(50/255,175/255,1)
+    love.graphics.setLineWidth(5)
+    
+end
+
+function love.update(dt)
+end
+
+
+function love.draw()
+   
+    love.graphics.draw(player, 0,400-105,0,0.3,0.3)
+    love.graphics.line(0,400,800,400)
+    
 end
